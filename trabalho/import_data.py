926c77ba043d3c8e7749796be7ef8b5649d4a109
Lucas Brum <lucasmatheuslegal@gmail.com>
2023-12-08 15:12:12 -0300

    opcao de classificacao binaria ao inves de multi

--- trabalho/import_data.py
+++ trabalho/import_data.py
@@ -2,15 +2,21 @@ from ucimlrepo import fetch_ucirepo
   
 # fetch dataset 
 heart_disease = fetch_ucirepo(id=45) 
-  
+
 # data (as pandas dataframes) 
 X = heart_disease.data.features 
 y = heart_disease.data.targets 
   
-# metadata 
-# print(heart_disease.metadata) 
-  
 # variable information 
-print(heart_disease.variables) 
+#print(heart_disease.variables)
 
+
+def replace_greater_than_zero(value):
+    return 1 if value > 0 else value
+
+y_result = y.applymap(replace_greater_than_zero)
+
+print("DataFrame Original (multiclasse):")
 print(y)
+print("\nDataFrame Após Substituição (classe binária):")
+print(y_result)
